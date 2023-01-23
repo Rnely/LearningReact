7845b3c7b2a5b58a870c76b74859ff8307755077
ricards <ricards.sinkjavics@accenture.com>
2023-01-23 20:17:28 +0200

    Cleaned up useFetch

--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -6,10 +6,12 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        const abortCont = new AbortController();
+
         //fetching data from json server
-        fetch(url)
+        fetch(url, { signal: abortCont.signal })
             .then(res => {
-                if(!res.ok) {
+                if (!res.ok) {
                     throw Error('could not fetch data for that resource');
                 }
                 return res.json();
@@ -20,9 +22,14 @@ const useFetch = (url) => {
                 setError(null);
             })
             .catch(err => {
-                setError(err.message);
-                setIsPending(false)
+                if (err.name === 'AbortError') {
+                    console.log('Fetch aborted')
+                } else {
+                    setError(err.message);
+                    setIsPending(false)
+                }
             })
+            return () => abortCont.abort();
         //dependencies
     }, [url]);
     
