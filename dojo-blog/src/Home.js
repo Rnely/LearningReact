95ed2e37e9eb4dc01cc5557b99f5f72890252cce
ricards <ricards.sinkjavics@accenture.com>
2023-01-23 19:02:53 +0200

    Fetched data from json server and added it to the blog

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,32 +2,24 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
-        { title: 'Welcome party', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'luigi', id: 3},
-        { title: 'Something random', body: 'lorem ipsum...', author: 'mario', id: 4}
-    ]);
-
-    const [name, setName] = useState('huh')
-
-    // Passing functions in other components as props
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
 
     useEffect(() => {
-        console.log('use effect ran');
-        //dependencie
-        console.log(name);
-    }, [name]);
+        //fetching data from json server
+        fetch('http://localhost:4000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                setBlogs(data);
+            })
+        //dependencies
+    }, []);
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-            <button onClick={() => setName('pffff')}>Change name</button>
-            <p>{ name} </p>
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
      );
 }
