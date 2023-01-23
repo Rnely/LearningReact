7e8749891da29707b5587b619453f266ec840458
ricards <ricards.sinkjavics@accenture.com>
2023-01-23 18:23:49 +0200

    Reused outputting lists by filter

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -5,12 +5,14 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
         { title: 'Welcome party', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'luigi', id: 3}
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'luigi', id: 3},
+        { title: 'Something random', body: 'lorem ipsum...', author: 'mario', id: 4}
     ]);
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs"/>
         </div>
      );
 }
