50bddff00df4a70c3da3589fce2902dd2aa1f78d
ricards <ricards.sinkjavics@accenture.com>
2023-01-23 17:36:42 +0200

    Tried out button click events

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Hello', e);
+    }
+
+    const handleClick2 = (name, e) => {
+        console.log('Hello ' + name, e.target );
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClick2('yahho', e)}>Click me again</button>
         </div>
      );
 }
