0883d8d66380105b4beaf5265fd3715dfba6a3d7
ricards <ricards.sinkjavics@accenture.com>
2023-01-23 19:27:03 +0200

    Handled fetch error

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,24 +4,32 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         //fetching data from json server
         fetch('http://localhost:4000/blogs')
             .then(res => {
+                if(!res.ok) {
+                    throw Error('could not fetch data for that resource');
+                }
                 return res.json();
             })
             .then(data => {
-                console.log(data);
                 setBlogs(data);
-                //Loading
                 setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setError(err.message);
+                setIsPending(false)
             })
         //dependencies
     }, []);
 
     return ( 
         <div className="home">
+            { error && <div>{ error }</div> }
             { isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
